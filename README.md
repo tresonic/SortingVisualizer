986d51cb885c5f88feafa12bc40d2363586455ca
Tresonic <tresonic@mail.de>
2022-01-30 10:21:49 +0100

    initial

---
+++ README.md
@@ -0,0 +1,32 @@
+SortingVisualizer
+==========================
+
+Building
+--------
+
+```sh
+mkdir build
+cd build
+cmake <path-to-cloned-repo>
+cmake --build .
+./src/imgui-sfml-example # or .\src\Debug\imgui-sfml-example.exe if you're on Windows
+```
+
+Options
+-------
+
+You can also set the following options when building:
+
+* `USE_SYSTEM_DEPS` (`OFF` by default) - search for SFML installed on system instead of fetching it
+* `LINK_DEPS_STATIC` (`ON` by default) - link dependencies statically
+
+Depencies
+---
+
+CMake >= 3.12
+
+Also, if you're on Linux, you'll need to install dependencies for building SFML
+specified
+[here](https://www.sfml-dev.org/tutorials/2.5/compile-with-cmake.php#installing-dependencies).
+
+sfml-imgui-template from [here](https://github.com/eliasdaler/imgui-sfml-fetchcontent/tree/dev)
---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+#include "SFML/Window/Keyboard.hpp"
+#include "imgui-SFML.h"
+#include "imgui.h"
+
+int main() {
+  sf::RenderWindow window(sf::VideoMode(1600, 900), "SortingVisualizer");
+  window.setFramerateLimit(60);
+  ImGui::SFML::Init(window);
+
+  sf::Clock deltaClock;
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      ImGui::SFML::ProcessEvent(event);
+
+      if (event.type == sf::Event::Closed ||
+          (event.type == sf::Event::KeyPressed &&
+           event.key.code == sf::Keyboard::Escape)) {
+        window.close();
+      }
+    }
+
+    ImGui::SFML::Update(window, deltaClock.restart());
+
+
+    window.clear();
+
+
+
+    ImGui::SFML::Render(window);
+    window.display();
+  }
+
+  ImGui::SFML::Shutdown();
+
+  return 0;
+}
