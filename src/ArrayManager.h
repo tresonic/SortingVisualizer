e0bdc2cc1e50ca9a3978bd7a3b0dbd47638fe161
Tresonic <tresonic@mail.de>
2022-02-01 08:55:06 +0100

    clang-format

--- src/ArrayManager.h
+++ src/ArrayManager.h
@@ -7,13 +7,14 @@
 
 class ArrayManager {
 public:
-  ArrayManager(sf::RenderWindow &win, uint32_t len);
-  void renderArray();
+    ArrayManager(sf::RenderWindow& win, uint32_t len);
+
+    void renderArray();
 
 private:
-  static uint32_t map(uint32_t x, uint32_t in_min, uint32_t in_max,
-                      uint32_t out_min, uint32_t out_max);
-  sf::RenderWindow &mWin;
-  std::vector<uint32_t> mArr;
-  std::vector<sf::Color> mCols;
+    static uint32_t map(uint32_t x, uint32_t in_min, uint32_t in_max, uint32_t out_min,
+                        uint32_t out_max);
+    sf::RenderWindow& mWin;
+    std::vector<uint32_t> mArr;
+    std::vector<sf::Color> mCols;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -9,37 +9,37 @@
 
 #include "ArrayManager.h"
 
-int main() {
-  sf::RenderWindow window(sf::VideoMode(1600, 900), "SortingVisualizer");
-  window.setFramerateLimit(60);
-  ImGui::SFML::Init(window);
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1600, 900), "SortingVisualizer");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
 
-  ArrayManager aRenderer{window, 100};
+    ArrayManager aRenderer{window, 100};
 
-  sf::Clock deltaClock;
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      ImGui::SFML::ProcessEvent(event);
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
 
-      if (event.type == sf::Event::Closed ||
-          (event.type == sf::Event::KeyPressed &&
-           event.key.code == sf::Keyboard::Escape)) {
-        window.close();
-      }
+            if (event.type == sf::Event::Closed ||
+                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        window.clear();
+
+        aRenderer.renderArray();
+
+        ImGui::SFML::Render(window);
+        window.display();
     }
 
-    ImGui::SFML::Update(window, deltaClock.restart());
+    ImGui::SFML::Shutdown();
 
-    window.clear();
-
-    aRenderer.renderArray();
-
-    ImGui::SFML::Render(window);
-    window.display();
-  }
-
-  ImGui::SFML::Shutdown();
-
-  return 0;
+    return 0;
 }
