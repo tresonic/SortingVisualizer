0e43f32bc5f42e68d79a024c73f69c6afc77adc7
Tresonic <tresonic@mail.de>
2022-02-03 23:02:53 +0100

    small gui improvements

--- src/ConfigWindow.cpp
+++ src/ConfigWindow.cpp
@@ -1,26 +1,30 @@
 #include "ConfigWindow.h"
 
-#include <thread>
 #include <imgui.h>
+#include <thread>
 
 #include "SortAlgs.h"
 
-void ConfigWindow::showConfigWindow() {
-    ImGui::Begin("SortingVisualizerConfig");
-    ImGui::SliderInt("Elements", &mNumElements, 5, 500);
-    ImGui::SliderInt("Delay", &mDelay, 0, 100);
-    ImGui::ListBox("Algorithm", &mSelectedSort, ALGO_NAMES, IM_ARRAYSIZE(ALGO_NAMES));
-    mStartSort = ImGui::Button("Start!");
+void ConfigWindow::showConfigWindow()
+{
+    ImGuiWindowFlags winFlags;
+    winFlags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_AlwaysAutoResize |
+               ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoTitleBar;
+    ImGui::Begin("SortingVisualizerConfig", nullptr, winFlags);
+    ImGui::InputInt("Elements", &mNumElements);
+    ImGui::InputInt("Delay", &mDelay);
+    ImGui::Combo("Algorithm", &mSelectedSort, ALGO_NAMES, IM_ARRAYSIZE(ALGO_NAMES));
+    mStartSort = ImGui::Button("Start");
     ImGui::End();
 }
 
-void ConfigWindow::applyToArrayManager(ArrayManager &arrMan) {
-    if(arrMan.getSize() != mNumElements)
-        arrMan.setSize(mNumElements);
+void ConfigWindow::applyToArrayManager(ArrayManager& arrMan)
+{
+    if (arrMan.getSize() != mNumElements) arrMan.setSize(mNumElements);
 
     arrMan.setDelay(mDelay);
 
-    if(mStartSort && !arrMan.isSorting()) {
+    if (mStartSort && !arrMan.isSorting()) {
         std::thread t{ALGO_FUNCS[mSelectedSort], std::ref(arrMan)};
         t.detach();
     }
