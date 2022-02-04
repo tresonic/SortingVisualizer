95d1f2ea292f1d7058be1f2bc5e5d4365484b4f4
Tresonic <tresonic@mail.de>
2022-02-04 10:48:57 +0100

    add more sorting algorithms

--- src/SortAlgs.cpp
+++ src/SortAlgs.cpp
@@ -1,8 +1,8 @@
 #include "SortAlgs.h"
 
-#include "ArrayManager.h"
 #include <array>
-#include <cstddef>
+
+#include "ArrayManager.h"
 
 bool SortAlgs::is_sorted(ArrayManager& arrMan)
 {
@@ -57,6 +57,34 @@ void SortAlgs::cocktail_shaker_sort(ArrayManager& arrMan)
     arrMan.finish();
 }
 
+void SortAlgs::insertion_sort(ArrayManager& arrMan)
+{
+    arrMan.start();
+    for (size_t i = 1; i < arrMan.getSize(); i++) {
+        for (size_t j = i; j > 0; j--) {
+            if (arrMan.get(j - 1) > arrMan.get(j))
+                arrMan.swap(j - 1, j);
+            else
+                break;
+        }
+    }
+    arrMan.finish();
+}
+
+void SortAlgs::inplace_merge_sort(ArrayManager& arrMan)
+{
+    arrMan.start();
+    inplace_merge_sort_recursive(arrMan, 0, arrMan.getSize() - 1);
+    arrMan.finish();
+}
+
+void SortAlgs::merge_sort(ArrayManager& arrMan)
+{
+    arrMan.start();
+    merge_sort_recursive(arrMan, 0, arrMan.getSize() - 1);
+    arrMan.finish();
+}
+
 void SortAlgs::bozosort(ArrayManager& arrMan)
 {
     arrMan.start();
@@ -71,6 +99,101 @@ void SortAlgs::quicksort(ArrayManager& arrMan)
     arrMan.start();
     quicksort_recursive(arrMan, 0, arrMan.getSize() - 1);
     arrMan.finish();
+}
+
+void SortAlgs::inplace_merge_sort_recursive(ArrayManager& arrMan, size_t lo, size_t hi)
+{
+    if (hi > lo) {
+        size_t mid = lo + (hi - lo) / 2;
+        inplace_merge_sort_recursive(arrMan, lo, mid);
+        inplace_merge_sort_recursive(arrMan, mid + 1, hi);
+        inplace_merge_sort_merge(arrMan, lo, mid, hi);
+    }
+}
+
+void SortAlgs::inplace_merge_sort_merge(ArrayManager& arrMan, size_t lo, size_t mid, size_t hi)
+{
+    size_t mid2 = mid + 1;
+
+    if (arrMan.get(mid) <= arrMan.get(mid2)) return;
+
+    while (lo <= mid && mid2 <= hi) {
+        if (arrMan.get(lo) <= arrMan.get(mid2)) {
+            lo++;
+        } else {
+            arr_type val = arrMan.get(mid2);
+            size_t idx = mid2;
+
+            while (idx != lo) {
+                arrMan.set(idx, arrMan.get(idx - 1));
+                idx--;
+            }
+            arrMan.set(lo, val);
+
+            lo++;
+            mid++;
+            mid2++;
+        }
+    }
+}
+
+void SortAlgs::merge_sort_recursive(ArrayManager& arrMan, size_t lo, size_t hi)
+{
+    if (hi > lo) {
+        size_t mid = lo + (hi - lo) / 2;
+        merge_sort_recursive(arrMan, lo, mid);
+        merge_sort_recursive(arrMan, mid + 1, hi);
+        merge_sort_merge(arrMan, lo, mid, hi);
+    }
+}
+
+void SortAlgs::merge_sort_merge(ArrayManager& arrMan, size_t left, size_t mid, size_t right)
+{
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+  
+    // Create temp arrays
+    std::vector<arr_type> leftArray;
+    std::vector<arr_type> rightArray;
+    leftArray.resize(subArrayOne);
+    rightArray.resize(subArrayTwo);
+  
+    // Copy data to temp arrays leftArray[] and rightArray[]
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = arrMan.get(left + i);
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = arrMan.get(mid + 1 + j);
+  
+    auto indexOfSubArrayOne = 0, // Initial index of first sub-array
+        indexOfSubArrayTwo = 0; // Initial index of second sub-array
+    int indexOfMergedArray = left; // Initial index of merged array
+  
+    // Merge the temp arrays back into array[left..right]
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+            arrMan.set(indexOfMergedArray, leftArray[indexOfSubArrayOne]);
+            indexOfSubArrayOne++;
+        }
+        else {
+            arrMan.set(indexOfMergedArray, rightArray[indexOfSubArrayTwo]);
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfSubArrayOne < subArrayOne) {
+        arrMan.set(indexOfMergedArray, leftArray[indexOfSubArrayOne]);
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        arrMan.set(indexOfMergedArray, rightArray[indexOfSubArrayTwo]);
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
 }
 
 void SortAlgs::quicksort_recursive(ArrayManager& arrMan, size_t lo, size_t hi)
--- src/SortAlgs.h
+++ src/SortAlgs.h
@@ -8,20 +8,31 @@ class SortAlgs {
 public:
     static void bubblesort(ArrayManager& arrMan);
     static void cocktail_shaker_sort(ArrayManager& arrMan);
+    static void insertion_sort(ArrayManager& arrMan);
+    static void inplace_merge_sort(ArrayManager& arrMan);
+    static void merge_sort(ArrayManager& arrMan);
     static void quicksort(ArrayManager& arrMan);
     static void bozosort(ArrayManager& arrMan);
 
     static bool is_sorted(ArrayManager& arrMan);
+
 private:
+    static void inplace_merge_sort_recursive(ArrayManager& arrMan, size_t lo, size_t hi);
+    static void inplace_merge_sort_merge(ArrayManager& arrMan, size_t lo, size_t mid, size_t hi);
+    static void merge_sort_recursive(ArrayManager& arrMan, size_t lo, size_t hi);
+    static void merge_sort_merge(ArrayManager& arrMan, size_t lo, size_t mid, size_t hi);
+
     static void quicksort_recursive(ArrayManager& arrMan, size_t lo, size_t hi);
     static size_t quicksort_partition(ArrayManager& arrMan, size_t lo, size_t hi);
 };
 
-static const char* ALGO_NAMES[] = {"Bubblesort", "Cocktail shaker sort", "Quicksort", "Bozosort"};
+static const char* ALGO_NAMES[] = {"Bubblesort",     "Cocktail shaker sort",
+                                   "Insertion sort", "Inplace merge sort",
+                                   "Merge sort",     "Quicksort",
+                                   "Bozosort"};
 
 static const std::function<void(ArrayManager&)> ALGO_FUNCS[] = {
-    SortAlgs::bubblesort,
-    SortAlgs::cocktail_shaker_sort,
-    SortAlgs::quicksort,
+    SortAlgs::bubblesort,         SortAlgs::cocktail_shaker_sort, SortAlgs::insertion_sort,
+    SortAlgs::inplace_merge_sort, SortAlgs::merge_sort,           SortAlgs::quicksort,
     SortAlgs::bozosort,
 };
